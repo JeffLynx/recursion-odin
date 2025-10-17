7781296e722b574f95b4e92f92ac3636718d764f
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-10-17 12:46:05 +0200

    Implement fib(num) with a recursive approach

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,11 +3,23 @@ def fib_iteration(num)
   return [0, 1] if num == 1
 
   arr = [0, 1]
-  (num - 2).times do
+  (num - 1).times do
     arr << (arr[-1] + arr[-2])
   end
   arr
 end
 
-example = fib_iteration(10)
-p example # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+# example = fib_iteration(10)
+# p example # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+def fib_recursion(num)
+  return [0] if num.zero?
+  return [0, 1] if num == 1
+
+  arr = fib_recursion(num - 1)
+  arr << arr[-1] + arr[-2]
+  arr
+end
+
+# example = fib_recursion(10)
+# p example # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
